1b7f5f082943c89d3104b2b7fb915bfa3cbecaec
Prateek Bhardwaj <prateekb1912@gmail.com>
2023-05-19 22:36:32 +0530

    Add get and post endpoints for /players route

--- routes/players.js
+++ routes/players.js
@@ -2,4 +2,34 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/')
+router.get('/', (req, res) => {
+    const page = req.query.page || 0;
+    const perPage = 10;
+
+    let players = [];
+
+    db.collection('players')
+        .find()
+        .sort({ matches_played: -1 }) // Returns a cursor pointing to a subset of the collection data
+        .skip(page*perPage)
+        .limit(perPage)
+        .forEach(plyr => players.push(plyr))
+        .then(() => {
+            res.status(200).json({"page": page, players})
+        })
+        .catch(() => {
+            res.status(500).json({ 'error': 'Internal Server Error' })
+        })
+})
+
+router.post('/', (req, res) => {
+    const newPlayer = req.body;
+
+    if(!newPlayer.name || !newPlayer.team)
+        res.status(400).json({'error': 'Please include name and team'})
+    
+    db.collection('players')
+    .insertOne(newPlayer)
+    .then(result => res.status(result))
+    .catch(err => res.status(500).json({'error': 'Internal Server Error', 'message': err}))
+})
